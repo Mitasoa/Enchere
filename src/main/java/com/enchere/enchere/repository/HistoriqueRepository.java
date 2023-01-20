c6c8bfe1acf44433360235f0296ad87d8d2dcc39
TanjonaRak <99284907+TanjonaRak@users.noreply.github.com>
2023-01-20 08:36:01 +0100

    Mondification
    HistoriqueUtilisateur

--- src/main/java/com/enchere/enchere/repository/HistoriqueRepository.java
+++ src/main/java/com/enchere/enchere/repository/HistoriqueRepository.java
@@ -29,8 +29,8 @@ public class HistoriqueRepository {
         // mongoTemplate
 
         Query query = new Query(Criteria.where("produitid").is(idproduit));
-        Update update = new Update().set("etatactuelle", 0);
-        mongoTemplate.updateFirst(query, update, HistoriqueRepository.class);
+        Update update = new Update().set("etatActuelle", 0);
+        mongoTemplate.updateFirst(query, update, HistoriqueUtilisateur.class);
     }
 
     public HistoriqueUtilisateur getDernierHistorique(int idproduit) {
