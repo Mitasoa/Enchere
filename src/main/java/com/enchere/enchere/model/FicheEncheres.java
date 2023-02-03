7da168a4001c2b4d306a3facf88ed93adfac21d7
TanjonaRak <99284907+TanjonaRak@users.noreply.github.com>
2023-02-03 22:35:24 +0100

    frontOffice

--- src/main/java/com/enchere/enchere/model/FicheEncheres.java
+++ src/main/java/com/enchere/enchere/model/FicheEncheres.java
@@ -89,7 +89,7 @@ public class FicheEncheres {
             // DAO.findAll();
             // DAO.
 
-            fiche.get(i).setSary(DAO.getPhoto());//// Integer.toString(fiche.get(i).getId())Sort.by({"id":fiche.get(i)})
+            fiche.get(i).setSary(DAO.getPhoto(this.id));//// Integer.toString(fiche.get(i).getId())Sort.by({"id":fiche.get(i)})
         }
     }
 
